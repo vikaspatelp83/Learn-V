5e119bd296cab4490e203f8eddf7235fd3e96030
Vikas Patel <vikaspatelp83@gmail.com>
2019-11-10 19:29:20 +0530

    Function square of a number

--- function.v
+++ function.v
@@ -1,10 +1,22 @@
 fn main(){
 	println(add(10,20))
 	println(sub(20,15))
+	println(square(10))
+	println(square(21))
+
 }
+
+
+
 fn add(/*inputs */ x int,y int) int{
 	return x+y
 }
 fn sub(x, y int) int{
 	return x-y
+}
+
+/* return multiple values */
+
+fn square(x int) int{
+	return x*x
 }
